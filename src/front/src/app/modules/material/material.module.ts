d1c66663cd8d5513cb7c92dc77fd61e175b60403
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2023-07-14 15:04:10 +0200

    I have made a views folder inside it I have created an add animal component and I have imported the MatModule inside app.module and I have exported in the material module. I have made the input structure in the html.

--- src/front/src/app/modules/material/material.module.ts
+++ src/front/src/app/modules/material/material.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
---
+++ src/front/src/app/views/addanimal/components/addanimal/addanimal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addanimal',
+  templateUrl: './addanimal.component.html',
+  styleUrls: ['./addanimal.component.scss']
+})
+export class AddanimalComponent {
+
+}
