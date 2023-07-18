d788b5b5bdfd1ad7ff93664c341de82c5c71083e
María <mariafsmk@gmail.com>
2023-07-18 10:49:29 +0200

    añadida ruta de animals edits en app routing

--- src/front/src/app/app-routing.module.ts
+++ src/front/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditAnimalViewComponent } from './views/edit-animal-view/edit-animal-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'animals/edit', component: EditAnimalViewComponent },];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
